27d9ce7696e15036f0b798aa4afe71a2aeea222d
alexwan <alexwan1122@gmail.com>
2014-03-12 17:36:40 +0200

    g

---
+++ cycles/src/cycles/Cycles.java
@@ -0,0 +1,45 @@
+package cycles;
+
+public class Cycles {
+		
+	public static void main(String args[]) {
+		
+		
+		
+		
+		int n;
+		for(n=10; n>0; n--)
+		System.out.println("такт " + n);
+		
+	
+	
+	int a, b;
+	
+	for(a=1, b=4; a<b; a++, b--) {
+	System.out.println("a = " + a) ;
+	System.out.println("b = " + b);
+	
+	}
+
+	
+	swing 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
